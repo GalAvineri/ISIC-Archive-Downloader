3a692deb3ec9930d2138a5aabe37fda196bb7d18
Gal Avineri <pudinda@gmail.com>
2017-11-02 23:38:53 +0200

    Handled none full downloads using image validation after the download. prettified get_ids to use json aswell

--- download_dataset.py
+++ download_dataset.py
@@ -1,10 +1,11 @@
-from download_dataset_subset import download_dataset_subset
+from download_dataset_subset import download_dataset_subset, validate_image
 import re
 import os
 import shutil
 import requests
 from os.path import join
 from threading import Thread
+from PIL import Image
 
 
 # Required Parameters:
@@ -13,38 +14,29 @@ size = 13786
 
 # Optional parameters:
 # Specify the path to the dir the images will be saved in
-images_dir = join('Data', 'images')
+images_dir = join(os.pardir, 'Data', 'Images')
 # Specify the path to the dir the descriptions will be saved in
-descs_dir = join('Data', 'descriptions')
+descs_dir = join(os.pardir, 'Data', 'Descriptions')
 # Choose the number of images each thread will download
 thread_subset_size = 300
 
 
 def main():
 
-    # Specify the path to the temporary text file which will hold the images ids
-    id_file_path = os.path.join(os.getcwd(), 'ids.txt')
-
     # If any of the images dir, descs dir or ids file exists - remove them so we won't override data
     # and perhaps create corrupted data
-    if os.path.isdir(images_dir):
-        shutil.rmtree(images_dir)
-    if os.path.isdir(descs_dir):
-        shutil.rmtree(descs_dir)
-    if os.path.isfile(id_file_path):
-        os.remove(id_file_path)
-    os.makedirs(images_dir)
-    os.makedirs(descs_dir)
+    create_or_recreate_dir(images_dir)
+    create_or_recreate_dir(descs_dir)
 
-    # 1. Get the ids of all the images into a txt file
-    get_ids(id_file_path)
+    # 1. Get the ids of all the images
+    ids = get_ids()
     # 2. Download all the images using their ids
-    download_dataset(id_file_path)
-    # 3. Cleanup
-    os.remove(id_file_path)
+    download_dataset(ids)
+    # 3. Validate all the images
+    validate_images()
 
 
-def get_ids(id_file_path):
+def get_ids():
 
     print('Collecting all images ids')
 
@@ -52,26 +44,15 @@ def get_ids(id_file_path):
     url = 'https://isic-archive.com/api/v1/image?limit={0}&offset=0&sort=name&sortdir=1'.format(size)
     # Get the images metadata
     response = requests.get(url, stream=True)
-    # extract all the ids
-    ids = []
-    for match in re.finditer('_id', response.content):
-        id = extract_id_from_line(response.content, match.start())
-        ids.append(id)
+    # Parse as json
+    meta_data = response.json()
+    # Extract all the ids
+    ids = [str(meta_data[index]['_id']) for index in range(size)]
 
-    # Write the ids into a txt file
-    with open(id_file_path, 'w') as f:
-        for item in ids:
-            f.write("%s\n" % item)
+    return ids
 
 
-def extract_id_from_line(s, index):
-    start_quotes = s.index('"', index+4)
-    end_quotes = s.index('"', start_quotes + 1)
-    id = s[start_quotes + 1 : end_quotes]
-    return id
-
-
-def download_dataset(id_file_path):
+def download_dataset(ids):
     # Determine the dataset subsets which multiple threads will download
     bins = range(0, size, thread_subset_size)
 
@@ -84,7 +65,7 @@ def download_dataset(id_file_path):
         if right_edge >= size:
             right_edge = size
         # Define the thread on the current subset
-        thread = Thread(target=download_dataset_subset, kwargs={'start': left_edge, 'end': right_edge, 'id_file_path': id_file_path,
+        thread = Thread(target=download_dataset_subset, kwargs={'start': left_edge, 'end': right_edge, 'ids': ids[left_edge: right_edge],
                                                                 'images_dir': images_dir, 'descs_dir': descs_dir, 'thread_id': idx})
         # Start it and add it to the list of threads
         thread.start()
@@ -95,6 +76,24 @@ def download_dataset(id_file_path):
         thread.join()
 
     print('All threads have finished')
+
+
+def validate_images():
+    # We would like to check that all the images are valid
+    try:
+        for image in os.listdir(images_dir):
+            image_path = join(images_dir, image)
+            validate_image(image_path)
+    except IOError as e:
+        print(e.message)
+        print("The image {0} wasn't downloaded successfully. "
+              "Please Open an issue in the github repository together with the error".format(image))
+
+
+def create_or_recreate_dir(dir_path):
+    if os.path.exists(dir_path):
+        shutil.rmtree(dir_path)
+    os.makedirs(dir_path)
 
 
 if __name__ == '__main__':
--- download_dataset_subset.py
+++ download_dataset_subset.py
@@ -2,10 +2,13 @@ import os
 import shutil
 import time
 import requests
+from urllib3.exceptions import ReadTimeoutError
+from requests.exceptions import RequestException
 import json
+from PIL import Image
 
 
-def download_dataset_subset(start, end, id_file_path, images_dir, descs_dir, thread_id):
+def download_dataset_subset(start, end, ids, images_dir, descs_dir, thread_id):
     # Announce the start of the thread (mainly for debugging)
     print('Thread {0} started'.format(thread_id))
 
@@ -13,52 +16,62 @@ def download_dataset_subset(start, end, id_file_path, images_dir, descs_dir, thr
     base_url_prefix = 'https://isic-archive.com/api/v1/image/'
     base_url_suffix = '/download?contentDisposition=inline'
 
-    with open(id_file_path) as f:
-        # get all the ids
-        ids = f.readlines()
+    # for each id:
+    # 1. get the corresponding image and description urls
+    # 2. download the image and the description from the urls
+    for id, index in zip(ids, range(start, end)):
 
-        # for each id:
-        # 1. get the corresponding image and description urls
-        # 2. download the image and the description from the urls
-        for index, id in enumerate(ids[start:end]):
-            # Update the index to the right offset of the bin we're working on
-            index = index + start
+        print("downloading image ({0} / {1} / {2})".format(start, index, end-1))
 
-            print("downloading image ({0} / {1} / {2})".format(start, index, end-1))
-            # strip the id from the \n at the end
-            id = id.strip()
+        # get the corresponding image url
+        url_image = base_url_prefix + id + base_url_suffix
+        # get the corresponding description url
+        url_desc = base_url_prefix + id
+        print ("url_image = {0}".format(url_image))
 
-            # get the corresponding image url
-            url_image = base_url_prefix + id + base_url_suffix
-            # get the corresponding description url
-            url_desc = base_url_prefix + id
-            print ("url_image = {0}".format(url_image))
+        # Download the image and desctiprion using the urls
+        # Sometimes their site isn't responding well, and than an error occurs,
+        # So we will retry the same url 5 seconds later, until it succeeds
+        succeeded = False
+        while not succeeded:
+            try:
+                response_image = requests.get(url_image, stream=True, timeout=20)
+                response_desc = requests.get(url_desc, stream=True, timeout=20)
+                response_image.raise_for_status()
+                response_desc.raise_for_status()
 
-            # Download the image and desctiprion using the urls
-            # Sometimes their site isn't responding well, and than an error occurs,
-            # So we will retry the same url 5 seconds later, until it succeeds
-            succeeded = False
-            while not succeeded:
-                try:
-                    response_image = requests.get(url_image, stream=True, timeout=20)
-                    response_desc = requests.get(url_desc, stream=True, timeout=20)
-                    response_image.raise_for_status()
-                    response_desc.raise_for_status()
+                # Write the image into a file
+                img_path = os.path.join(images_dir, 'ISIC_{0}.jpg'.format(str(index).zfill(7)))
+                with open(img_path, 'wb') as imageFile:
+                    shutil.copyfileobj(response_image.raw, imageFile)
 
-                    # Write the image into a file
-                    img_path = os.path.join(images_dir, 'ISIC_{0}.jpg'.format(str(index).zfill(7)))
-                    with open(img_path, 'wb') as imageFile:
-                        shutil.copyfileobj(response_image.raw, imageFile)
+                # Validate the image was downloaded correctly
+                validate_image(img_path)
 
-                    # Parse the description and write the description into a file
-                    parsed_desc = response_desc.json()
-                    desc_path = os.path.join(descs_dir, 'ISIC_{0}'.format(str(index).zfill(7)))
-                    with open(desc_path, 'w') as descFile:
-                        json.dump(parsed_desc, descFile, indent=2)
+                # Parse the description and write the description into a file
+                parsed_desc = response_desc.json()
+                desc_path = os.path.join(descs_dir, 'ISIC_{0}'.format(str(index).zfill(7)))
+                with open(desc_path, 'w') as descFile:
+                    json.dump(parsed_desc, descFile, indent=2)
 
-                    succeeded = True
-                except:
-                    print "trying again image number ", index
-                    time.sleep(20)
+                succeeded = True
+            except RequestException:
+                print("connection error occurred - trying again image number {0}".format(index))
+                time.sleep(10)
+            except ReadTimeoutError:
+                print("connection timed out - trying again image number {0}".format(index))
+                time.sleep(10)
+            except IOError:
+                print("Image wasn't fully downloaded - trying again image number {0}".format(index))
+                time.sleep(10)
 
     print('Thread {0} finished'.format(thread_id))
+
+
+def validate_image(image_path):
+    # We would like to validate the image was fully downloaded and wasn't truncated.
+    # To do so, we can open the image file using PIL.Image and try to resize it to the size
+    # the file declares it has.
+    # If the image wasn't fully downloaded and was trucnated - an error will be raised.
+    img = Image.open(image_path)
+    img.resize(img.size)
